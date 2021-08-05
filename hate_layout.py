241c0761a68ce3e1cfbdf611442eabd5052fc271
Alan Morgan <alanxoc3@gmail.com>
2021-08-05 16:47:47 -0600

    Reformated layout. Fixed a few keys. Redid most symbols, in a smart way.

--- hate_layout.py
+++ hate_layout.py
@@ -1,107 +1,73 @@
 from pynput.keyboard import Key
 
 hate_layout = {
+        "a"      : "k",
         "b"      : "l",
         "c"      : "e",
         "d"      : "t",
         "e"      : "a",
         "f"      : "h",
         "g"      : "j",
-        "a"      : "k",
 
+        "ab"     : Key.backspace,
+        "ac"     : "n",
+        "ad"     : "d",
+        "ae"     : "p",
+        "af"     : "g",
+        "ag"     : "z",
         "bc"     : "v",
         "bd"     : "c",
         "be"     : "w",
         "bf"     : "y",
         "bg"     : "u",
-        "ab"     : Key.backspace,
         "cd"     : "x",
         "ce"     : "o",
         "cf"     : "i",
         "cg"     : Key.space,
-        "ac"     : "n",
         "de"     : "q",
         "df"     : "s",
         "dg"     : "r",
-        "ad"     : "d",
         "ef"     : "b",
         "eg"     : "m",
-        "ae"     : "p",
         "fg"     : "f",
-        "af"     : "g",
-        "ag"     : "z",
 
+        # Some things to assign...
+        # - = Key.insert Key.delete
+        "abc"    : "", # unassigned
+        "abd"    : "", # unassigned
+        "abe"    : "", # unassigned
+        "abf"    : "", # unassigned
+        "abg"    : "", # unassigned
+        "acd"    : "", # unassigned
+        "ace"    : Key.tab,
+        "acf"    : "", # unassigned
+        "acg"    : "", # unassigned
+        "ade"    : "", # unassigned
+        "adf"    : "", # unassigned
+        "adg"    : "", # unassigned
+        "aef"    : "", # unassigned
+        "aeg"    : "", # unassigned
+        "afg"    : Key.esc,
         "bcd"    : "1",
         "bce"    : "2",
         "bcf"    : "3",
         "bcg"    : "4",
-        "abc"    : "", # unassigned
         "bde"    : "5",
         "bdf"    : "6",
         "bdg"    : "7",
-        "abd"    : "", # unassigned
         "bef"    : "8",
         "beg"    : "9",
-        "abe"    : "", # unassigned
         "bfg"    : "0",
-        "abf"    : "-",
-        "abg"    : "=",
-        "cde"    : "`",
-        "cdf"    : "[",
-        "cdg"    : "]",
-        "acd"    : "/",
-        "cef"    : ";",
-        "ace"    : "'",
-        "cfg"    : "\\",
-        "acf"    : ",",
-        "cge"    : ".",
-        "acg"    : "", # Key.esc
-        "def"    : "", # Key.tab
-        "deg"    : "", # Key.enter
-        "ade"    : "", # Key.insert
-        "dfg"    : "", # Key.delete
-        "adf"    : "", # unassigned
-        "adg"    : "", # unassigned
-        "efg"    : "", # unassigned
-        "aef"    : "", # unassigned
-        "aeg"    : "", # unassigned
-        "afg"    : "", # unassigned
-
-        "bcde"   : "", # unassigned
-        "bcdf"   : "", # unassigned
-        "bcdg"   : "", # unassigned
-        "abcd"   : "", # unassigned
-        "bcef"   : "", # unassigned
-        "bceg"   : "", # unassigned
-        "abce"   : "", # unassigned
-        "bcfg"   : "", # unassigned
-        "abcf"   : "", # unassigned
-        "abcg"   : "", # unassigned
-        "bdef"   : "", # unassigned
-        "bdeg"   : "", # unassigned
-        "abde"   : "", # unassigned
-        "bdfg"   : "", # unassigned
-        "abdf"   : "", # unassigned
-        "abdg"   : "", # unassigned
-        "befg"   : "", # unassigned
-        "abef"   : "", # unassigned
-        "abeg"   : "", # unassigned
-        "abfg"   : "", # unassigned
-        "cdef"   : "", # unassigned
-        "cdeg"   : "", # unassigned
-        "acde"   : "", # unassigned
-        "cdfg"   : "", # unassigned
-        "acdf"   : "", # unassigned
-        "acdg"   : "", # unassigned
-        "cefg"   : "", # unassigned
-        "acef"   : "", # unassigned
-        "aceg"   : "", # unassigned
-        "acfg"   : "", # unassigned
-        "defg"   : "", # unassigned
-        "adef"   : "", # unassigned
-        "adeg"   : "", # unassigned
-        "adfg"   : "", # unassigned
-        "aefg"   : "", # unassigned
+        "cde"    : "[",
+        "cdf"    : "/",
+        "cdg"    : ",",
+        "cef"    : "'",
+        "ceg"    : Key.enter,
+        "cfg"    : ".",
+        "def"    : ";",
+        "deg"    : "`",
+        "dfg"    : "\\",
+        "efg"    : "]",
 
         # 5 black keys
         "0"      : Key.alt,
