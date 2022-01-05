e956234faa4ce52539caa22d611407273d791877
Alan Morgan <alanxoc3@gmail.com>
2022-01-04 20:48:15 -0700

    Modularized the code a bit. Broke things up into separate functions.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,40 @@
 extern crate midir;
 
-use std::error::Error;
 use std::thread;
 use hatel::LAYOUT;
 use hatel::NUM_TO_NOTE;
 
-use midir::{MidiInput, Ignore};
+use midir::{MidiInput, Ignore, MidiInputConnection};
 
 fn num_to_note(note_num: u8) -> &'static str {
     NUM_TO_NOTE.get(&(note_num % 12)).unwrap_or(&"invalid")
 }
 
 fn main() {
-    match run() {
-        Ok(_) => (),
-        Err(err) => println!("Error: {}", err)
+    let mut conn_vec = refresh_connections();
+
+    println!("Listening on all midi in connections:");
+    for i in 0..conn_vec.len() {
+        println!("{}: {}", &conn_vec[i].portnum, &conn_vec[i].portname);
     }
+
+    thread::park();
+
+    for midichan in conn_vec.drain(..) {
+        midichan.connection.close();
+    }
+
+    println!("Closing connection");
 }
 
-fn run() -> Result<(), Box<dyn Error>> {
-    let mut midi_in = MidiInput::new("midir reading input")?;
+struct MidiChannel {
+    connection: MidiInputConnection<()>,
+    portname: String,
+    portnum: usize,
+}
+
+fn refresh_connections() -> Vec<MidiChannel> {
+    let mut midi_in = MidiInput::new("midir reading input").unwrap();
     midi_in.ignore(Ignore::None);
 
     // I can use "port" or "get_name" to check the current state. There is also a close method for MidiInputConnection. See:
@@ -27,35 +42,29 @@ fn run() -> Result<(), Box<dyn Error>> {
     let in_ports = midi_in.ports();
     let mut conn_vec = Vec::new();
 
-    if in_ports.len() == 0 {
-        return Err("no input port found".into())
-    }
-
-    println!("Listening on all midi in connections:");
     for i in 0..in_ports.len() {
-        let mut local_midi_in = MidiInput::new("midir reading input")?;
+        let mut local_midi_in = MidiInput::new("midir reading input").unwrap();
         local_midi_in.ignore(Ignore::None);
 
         let in_port = &in_ports[i];
-        let in_port_name = local_midi_in.port_name(in_port)?;
+        let in_port_name = local_midi_in.port_name(in_port).unwrap();
 
         let conn = local_midi_in.connect(in_port, "midi-port", |stamp, message, _| {
-            println!("{}: {:?} (len = {})", stamp, message, message.len());
-            if message.len() >= 2 {
-                let note = num_to_note(message[1]);
-                println!("{}: {}", note, LAYOUT.get(&note).unwrap_or(&"invalid"));
-            }
+            midi_listener(stamp, &message);
         }, ());
 
-
         if conn.is_ok() {
-            println!("{}: {}", i, in_port_name);
-            conn_vec.push(conn);
+            conn_vec.push(MidiChannel { connection: conn.unwrap(), portname: in_port_name, portnum: i });
         }
     }
 
-    thread::park();
+    return conn_vec
+}
 
-    println!("Closing connection");
-    Ok(())
+fn midi_listener(stamp: u64, message: &[u8]) {
+    println!("{}: {:?} (len = {})", stamp, message, message.len());
+    if message.len() >= 2 {
+        let note = num_to_note(message[1]);
+        println!("{}: {}", note, LAYOUT.get(&note).unwrap_or(&"invalid"));
+    }
 }
