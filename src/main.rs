815cde538e0f6659c43ccaf164e09c3e4ddaea44
Alan Morgan <alanxoc3@gmail.com>
2022-02-12 12:48:44 -0700

    Whaaaat? Did someone ask for midi auto-connect?
    The rust version still doesn't replace the python one yet. A big reason I chose
    to do a rust rewrite was because the library supported connecting to midi
    sources while running. And I finally got that feature working!

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,89 @@
 extern crate midir;
 
+use std::collections::HashSet;
+use std::time::Duration;
+use std::collections::HashMap;
 use std::thread;
 use hatel::LAYOUT;
 use hatel::NUM_TO_NOTE;
 use std::process::Command;
 
-use midir::{MidiInput, Ignore, MidiInputConnection};
+use midir::{MidiInput, MidiInputConnection};
 
 fn num_to_note(note_num: u8) -> &'static str {
     NUM_TO_NOTE.get(&(note_num % 12)).unwrap_or(&"invalid")
 }
 
 fn main() {
-    let mut conn_vec = refresh_connections();
+    let mut input_map = HashMap::from([]);
 
-    println!("Listening on all midi in connections:");
-    for i in 0..conn_vec.len() {
-        println!("{}: {}", &conn_vec[i].portnum, &conn_vec[i].portname);
-    }
+    let scheduler = thread::spawn(move || {
+        loop {
+            let wait_time = Duration::from_millis(1000);
+            refresh_connections(&mut input_map);
+            thread::sleep(wait_time);
+        }
+    });
 
     thread::park();
 
-    for midichan in conn_vec.drain(..) {
-        midichan.connection.close();
-    }
-
-    println!("Closing connection");
+    // for midichan in conn_vec.drain(..) {
+    //     midichan.connection.close();
+    // }
 }
 
 struct MidiChannel {
     connection: MidiInputConnection<()>,
     portname: String,
-    portnum: usize,
 }
 
-fn refresh_connections() -> Vec<MidiChannel> {
-    let mut midi_in = MidiInput::new("midir reading input").unwrap();
-    midi_in.ignore(Ignore::None);
+fn refresh_connections(input_map: &mut HashMap<String, MidiChannel>) {
+    let mut touched_ports_map = HashMap::new();
 
-    // I can use "port" or "get_name" to check the current state. There is also a close method for MidiInputConnection. See:
-    // https://docs.rs/midir/0.7.0/midir/struct.MidiInput.html
-    let in_ports = midi_in.ports();
-    let mut conn_vec = Vec::new();
+    let init_midi = MidiInput::new("midir reading input").unwrap();
+    for in_port in init_midi.ports() {
+        let local_midi_in = MidiInput::new("midir reading input").unwrap();
+        let in_port_name = String::from(&local_midi_in.port_name(&in_port).unwrap());
+        touched_ports_map.insert(in_port_name, in_port);
+    }
 
-    for i in 0..in_ports.len() {
-        let mut local_midi_in = MidiInput::new("midir reading input").unwrap();
-        local_midi_in.ignore(Ignore::None);
+    let prev_ports: HashSet<String> = input_map.keys().cloned().collect();
+    let touched_ports: HashSet<String> = touched_ports_map.keys().cloned().collect();
+    for x in prev_ports.difference(&touched_ports) {
+        println!("Lost Connection: {}", x);
+        input_map.remove(x).unwrap().connection.close();
+    }
 
-        let in_port = &in_ports[i];
-        let in_port_name = local_midi_in.port_name(in_port).unwrap();
+    for port_name in touched_ports.difference(&prev_ports) {
+        println!("New Connection: {}", port_name);
+
+        let local_midi_in = MidiInput::new("midir reading input").unwrap();
+        let in_port = touched_ports_map.get(port_name).unwrap();
 
         let conn = local_midi_in.connect(in_port, "midi-port", |stamp, message, _| {
             midi_listener(stamp, &message);
         }, ());
 
         if conn.is_ok() {
-            conn_vec.push(MidiChannel { connection: conn.unwrap(), portname: in_port_name, portnum: i });
+            input_map.insert(port_name.clone(), MidiChannel { connection: conn.unwrap(), portname: port_name.clone() });
+        } else {
+            println!("Lost Connection: {}", port_name);
+            input_map.remove(port_name).unwrap().connection.close();
+        }
+    }
+}
+
+fn midi_listener(_stamp: u64, message: &[u8]) {
+    // println!("{}: {:?} (len = {})", stamp, message, message.len());
+    if message.len() >= 2 {
+        let note = num_to_note(message[1]);
+        if message[2] != 0 {
+            println!("{}: {}", note, LAYOUT.get(&note).unwrap_or(&"invalid"));
         }
     }
 
-    return conn_vec
-}
-
-fn midi_listener(stamp: u64, message: &[u8]) {
-    println!("{}: {:?} (len = {})", stamp, message, message.len());
-    if message.len() >= 2 {
-        let note = num_to_note(message[1]);
-        println!("{}: {}", note, LAYOUT.get(&note).unwrap_or(&"invalid"));
-    }
-
-    Command::new("echo")
-        .arg("hello")
-        .spawn()
-        .expect("echo failed");
+    // Command::new("echo")
+    //     .arg("hello")
+    //     .spawn()
+    //     .expect("echo failed");
 }
